e8177ae10fb109dfa0c7ccc635b3aef6bef7c281
Scott Schroeder <scottschroeder@sent.com>
2021-03-25 20:47:25 -0400

    support for expr lua functions

--- lua/vimrc/config/mapping.lua
+++ lua/vimrc/config/mapping.lua
@@ -7,22 +7,32 @@ local function new_binding(f)
 end
 
 local function call_bound(id)
-    bind_funcs[id]()
+    return bind_funcs[id]()
 end
+
+_G.call_bound = call_bound
 
 local function fmt_vim_call(id)
     -- local info = debug.getinfo(1,'S');
     return ":lua require('vimrc.config.mapping').call_bound(" .. id .. ")<CR>"
 end
 
+local function fmt_expr_call(id)
+    return "v:lua.call_bound(" .. id .. ")"
+end
+
 local function map(mode, lhs, rhs, opts)
-    local options = {noremap = true}
+    local options = {}
     if opts then
         options = vim.tbl_extend("force", options, opts)
     end
     if type(rhs) == "function" then 
         local id = new_binding(rhs)
-        rhs = fmt_vim_call(id)
+        if options.expr == true then
+            rhs = fmt_expr_call(id)
+        else
+            rhs = fmt_vim_call(id)
+        end
         options = vim.tbl_extend("force", options, {silent=true})
     end
     vim.api.nvim_set_keymap(mode, lhs, rhs, options)
