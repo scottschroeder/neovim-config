e364d43e25dbd369dabe3c18e89c1fa9cee33e39
Scott Schroeder <scottschroeder@sent.com>
2021-11-30 09:18:33 -0500

    trying out luasnip

---
+++ lua/vimrc/plugins/luasnip.lua
@@ -0,0 +1,36 @@
+local log = require("vimrc.log")
+local has_luasnip, ls = pcall(require, "luasnip")
+if not has_luasnip then
+  log.warn("luasnip not installed")
+  return
+end
+
+local s = ls.snippet
+local sn = ls.snippet_node
+local isn = ls.indent_snippet_node
+local t = ls.text_node
+local i = ls.insert_node
+local f = ls.function_node
+local c = ls.choice_node
+local d = ls.dynamic_node
+local r = ls.restore_node
+local events = require("luasnip.util.events")
+
+-- Returns a snippet_node wrapped around an insert_node whose initial
+-- text value is set to the current date in the desired format.
+local date_input = function(args, state, fmt)
+  local fmt = fmt or "%Y-%m-%d"
+  return sn(nil, i(1, os.date(fmt)))
+end
+
+ls.snippets = {
+  all = {
+    s("novel", {
+      t("It was a dark and stormy night on "),
+      d(1, date_input, {}, "%A, %B %d of %Y"),
+      t(" and the clocks were striking thirteen."),
+    }),
+  }
+}
+
+log.trace(ls.snippets)
--- lua/vimrc/plugins/nvim-cmp.lua
+++ lua/vimrc/plugins/nvim-cmp.lua
@@ -21,6 +21,12 @@ vim.api.nvim_set_keymap(
   { noremap = true }
 )
 
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+local luasnip = require("luasnip")
 
 local lspkind = require "lspkind"
 lspkind.init()
@@ -64,10 +70,33 @@ cmp.setup {
     -- },
 
     -- Testing
-    ["<tab>"] = cmp.mapping.confirm {
+    ["<CR>"] = cmp.mapping.confirm {
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
+      { "i"}
     },
+
+     ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
 
     -- If you want tab completion :'(
     --  First you have to just promise to read `:help ins-completion`.
