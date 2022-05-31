961abf62daabbfb9d9fb6a2e34d802609d5d8c76
Scott Schroeder <scottschroeder@sent.com>
2022-05-31 10:02:12 -0400

    show proper spaces in heirline diagnostics

--- lua/vimrc/plugins/heirline/diagnostic.lua
+++ lua/vimrc/plugins/heirline/diagnostic.lua
@@ -5,11 +5,19 @@ local M = {}
 
 -- TODO this should be set already, we shouldn't need to default
 local function get_defined_sign_or(name, default)
-    local sign_info = vim.fn.sign_getdefined(name)
-    if sign_info and #sign_info > 1 then
-      return sign_info[1].text
-    end
-    return default
+  local sign_info = vim.fn.sign_getdefined(name)
+  if sign_info and #sign_info > 1 then
+    return sign_info[1].text
+  end
+  return default
+end
+
+local function append_space_if(text, condition)
+  if condition then
+    return text .. " "
+  else
+    return text
+  end
 end
 
 M.Diagnostics = {
@@ -29,6 +37,11 @@ M.Diagnostics = {
     self.warnings = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.WARN })
     self.hints = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.HINT })
     self.info = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.INFO })
+
+    self.infospace = self.hints > 0
+    self.warnspace = self.info > 0 or self.infospace
+    self.errorspace = self.warnings > 0 or self.warnspace
+
   end,
 
   {
@@ -43,19 +56,19 @@ M.Diagnostics = {
   {
     provider = function(self)
       -- 0 is just another output, we can decide to print it or not!
-      return self.errors > 0 and (self.error_icon .. self.errors .. " ")
+      return self.errors > 0 and (append_space_if(self.error_icon .. self.errors, self.errorspace))
     end,
     hl = { fg = colors.diag.error },
   },
   {
     provider = function(self)
-      return self.warnings > 0 and (self.warn_icon .. self.warnings .. " ")
+      return self.warnings > 0 and (append_space_if(self.warn_icon .. self.warnings, self.warnspace))
     end,
     hl = { fg = colors.diag.warn },
   },
   {
     provider = function(self)
-      return self.info > 0 and (self.info_icon .. self.info .. " ")
+      return self.info > 0 and (append_space_if(self.info_icon .. self.info, self.infospace))
     end,
     hl = { fg = colors.diag.info },
   },
