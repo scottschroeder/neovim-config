b646f3c25118dbbd5329da012079210735aaea18
Scott Schroeder <scottschroeder@sent.com>
2023-02-05 12:09:26 -0500

    add dd repos to project

--- lua/vimrc/plugins/project.lua
+++ lua/vimrc/plugins/project.lua
@@ -1,10 +1,11 @@
 require("vimrc.project").setup({
-  git_roots = {"~/src"},
+  git_roots = {"~/src", "~/dd"},
   extras = {
     {title="$HOME", path="~"},
     {title="shell", path="~/.shell"},
     {title="Org Mode", path="~/Dropbox/Documents/Org"},
     {title="New Org Mode", path="~/Dropbox/Documents/NewOrg"},
+    {title="DD Hack", path="~/src/ddlocal/scottschroeder/ddhack"},
     -- {path="~"},
     -- {title="Neovim Config", path="~/src/github/scottschroeder/neovim-config"},
     -- {path="~/src/github/scottschroeder/neovim-config"},
