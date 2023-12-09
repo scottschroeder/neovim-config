2aa9486c83999a8b445d79937fac1fd7abe0988d
Scott Schroeder <scottschroeder@sent.com>
2023-12-09 09:24:57 -0600

    move autosave back to main repo

--- lua/vimrc/plugins/auto-save.lua
+++ lua/vimrc/plugins/auto-save.lua
@@ -6,4 +6,23 @@ if not ok then
   return
 end
 
-autosave.setup({})
+local is_autosave = function(buf)
+  local utils = require("auto-save.utils.data")
+  local filepath = vim.api.nvim_buf_get_name(buf)
+  local filename = vim.fs.basename(filepath)
+  if filename == "Cargo.toml" then
+    return false
+  end
+
+  if
+      vim.fn.getbufvar(buf, "&modifiable") == 1 and
+      utils.not_in(vim.fn.getbufvar(buf, "&filetype"), {"oil"}) then
+    return true -- met condition(s), can save
+  end
+  return false  -- can't save
+end
+
+autosave.setup({
+  write_all_buffers = true,
+  condition = is_autosave,
+})
