1737c5e7806649dd009301a162fe9808714e4806
Scott Schroeder <scottschroeder@sent.com>
2023-12-09 11:11:08 -0600

    Add keybindings for tab controls

--- lua/vimrc/plugins/tabby.lua
+++ lua/vimrc/plugins/tabby.lua
@@ -1,23 +1,26 @@
 local log = require("vimrc.log")
 local map = require("vimrc.config.mapping").map
 local map_prefix = require("vimrc.config.mapping").prefix
-local ok, tabby = pcall(require,"tabby")
-if not ok then
-  log.warn("unable to load tabby")
-  return
-end
+-- local ok, tabby = pcall(require,"tabby")
+-- if not ok then
+--   log.warn("unable to load tabby")
+--   return
+-- end
 
-tabby.setup({
-  tabline = require("tabby.presets").active_wins_at_tail
-})
+-- tabby.setup({
+--   tabline = require("tabby.presets").active_wins_at_tail
+-- })
 
 map_prefix("<leader>t", "Tabs")
-map("n", "<leader>ta", ":$tabnew<CR>", {noremap = true})
-map("n", "<leader>tc", ":tabclose<CR>", { noremap = true })
-map("n", "<leader>to", ":tabonly<CR>", { noremap = true })
-map("n", "<leader>tn", ":tabn<CR>", { noremap = true })
-map("n", "<leader>tp", ":tabp<CR>", { noremap = true })
-map("n", "<S-Right>", ":tabn<CR>", { noremap = true })
-map("n", "<S-Left>", ":tabp<CR>", { noremap = true })
-map("n", "<leader>tmp", ":-tabmove<CR>", { noremap = true })
-map("n", "<leader>tmn", ":+tabmove<CR>", { noremap = true })
+map("n", "<leader>ta", ":$tabnew<CR>", {noremap = true, desc = "New Tab"})
+map("n", "<leader>tc", ":tabclose<CR>", { noremap = true, desc = "Close Tab"})
+map("n", "<leader>to", ":tabonly<CR>", { noremap = true, desc = "Close all other tabs" })
+-- map("n", "<leader>tn", ":tabn<CR>", { noremap = true })
+-- map("n", "<leader>tp", ":tabp<CR>", { noremap = true })
+-- map("n", "<S-Right>", ":tabn<CR>", { noremap = true })
+-- map("n", "<S-Left>", ":tabp<CR>", { noremap = true })
+
+map("n", "]t", ":tabn<CR>", { noremap = true, desc = "TabNext"})
+map("n", "[t", ":tabp<CR>", { noremap = true, desc = "TabPrev"})
+-- map("n", "<leader>tmp", ":-tabmove<CR>", { noremap = true })
+-- map("n", "<leader>tmn", ":+tabmove<CR>", { noremap = true })
