0332777faab2009ee63514615ae934395b2ceb6c
Scott Schroeder <scottschroeder@sent.com>
2025-01-12 15:20:00 -0600

    automanage go imports on save

--- lua/lsp/lang/golang.lua
+++ lua/lsp/lang/golang.lua
@@ -3,6 +3,24 @@
 require('lspconfig').gopls.setup {
   on_attach = function(c, b)
     require("lsp.attach").on_attach(c, b)
+
+    vim.api.nvim_create_autocmd("BufWritePre",
+      {
+        buffer = b,
+        callback = function()
+          local params = vim.lsp.util.make_range_params()
+          params.context = { only = { "source.organizeImports" } }
+          local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 1000)
+          for _, res in pairs(result or {}) do
+            for _, action in pairs(res.result or {}) do
+              if action.edit then
+                vim.lsp
+                    .util.apply_workspace_edit(action.edit, "utf-8")
+              end
+            end
+          end
+        end,
+      })
   end,
   capabilities = require("lsp.capabilities").capabilities,
   settings = {
