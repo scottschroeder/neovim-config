d2fc971a3c0554c5972f16a3849237bff61ce90d
Scott Schroeder <scottschroeder@sent.com>
2025-02-09 11:05:53 -0600

    make golang autoimport organization toggleable

--- lua/lsp/lang/golang.lua
+++ lua/lsp/lang/golang.lua
@@ -1,13 +1,30 @@
 -- local ok_inlay_hints, inlay_hints = pcall(require, "inlay-hints")
+--
+local do_import_on_save = true
 
 require('lspconfig').gopls.setup {
   on_attach = function(c, b)
     require("lsp.attach").on_attach(c, b)
+    local buf_map = require('rc.utils.map').buf_map
+
+    local toggle_import_on_save = function()
+      do_import_on_save = not do_import_on_save
+      if do_import_on_save then
+        vim.notify("Organize imports on save: ON")
+      else
+        vim.notify("Organize imports on save: OFF")
+      end
+    end
+
+    buf_map(b, "n", "<leader>li", toggle_import_on_save, { desc = "Toggle organize imports on save" })
 
     vim.api.nvim_create_autocmd("BufWritePre",
       {
         buffer = b,
         callback = function()
+          if not do_import_on_save then
+            return
+          end
           local params = vim.lsp.util.make_range_params()
           params.context = { only = { "source.organizeImports" } }
           local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 1000)
@@ -25,6 +42,7 @@ require('lspconfig').gopls.setup {
   capabilities = require("lsp.capabilities").capabilities,
   settings = {
     gopls = {
+      gofumpt = true,
       experimentalPostfixCompletions = true,
       analyses = {
         unusedparams = true,
