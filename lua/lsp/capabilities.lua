cb67ed784be7e2de9e7bc903c3b97319700b28ab
Scott Schroeder <scottschroeder@sent.com>
2025-03-29 16:31:33 -0500

    add blink cmp

--- lua/lsp/capabilities.lua
+++ lua/lsp/capabilities.lua
@@ -5,6 +5,12 @@ local gen_cap = function()
   if ok then
     capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
   end
+
+  local blink_ok, blink_cmp = pcall(require, "blink.cmp")
+  if blink_ok then
+    blink_cmp.get_lsp_capabilities(capabilities)
+  end
+
   return capabilities
 end
 
---
+++ lua/rc/plugins/blink.lua
@@ -0,0 +1,81 @@
+-- TODO
+-- Copilot suggestions to be at the end of the list
+return {
+  'saghen/blink.cmp',
+  -- optional: provides snippets for the snippet source
+  dependencies = {
+    "fang2hou/blink-copilot",
+    { 'L3MON4D3/LuaSnip', version = 'v2.*' },
+  },
+
+  -- use a release tag to download pre-built binaries
+  version = '1.*',
+  config = function()
+    local cmp = require('blink.cmp')
+    cmp.setup({
+      enabled = function()
+        -- return not vim.tbl_contains({ "markdown" }, vim.bo.filetype)
+        return true
+      end,
+
+      -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+      -- 'super-tab' for mappings similar to vscode (tab to accept)
+      -- 'enter' for enter to accept
+      -- 'none' for no mappings
+      --
+      -- All presets have the following mappings:
+      -- C-space: Open menu or open docs if already open
+      -- C-n/C-p or Up/Down: Select next/previous item
+      -- C-e: Hide menu
+      -- C-k: Toggle signature help (if signature.enabled = true)
+      --
+      -- See :h blink-cmp-config-keymap for defining your own keymap
+      keymap = { preset = 'default' },
+
+      appearance = {
+        -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+        -- Adjusts spacing to ensure icons are aligned
+        nerd_font_variant = 'mono'
+      },
+
+      -- (Default) Only show the documentation popup when manually triggered
+      completion = {
+        documentation = { auto_show = true },
+        ghost_text = { enabled = true }, -- does this have any effect, or just visual?
+        menu = {
+          draw = {
+            columns = { { "label", "label_description", gap = 1 }, { "kind_icon", "kind", "source_name", gap = 1 } },
+          }
+        }
+      },
+
+      -- Default list of enabled providers defined so that you can extend it
+      -- elsewhere in your config, without redefining it, due to `opts_extend`
+      sources = {
+        default = { 'lsp', 'path', 'snippets', 'buffer', 'copilot' },
+        providers = {
+          copilot = {
+            name = "AI",
+            module = "blink-copilot",
+            score_offset = -4,
+            async = true,
+          },
+        },
+      },
+      snippets = { preset = 'luasnip' },
+      signature = { enabled = true },
+      -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+      -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+      -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+      --
+      -- See the fuzzy documentation for more information
+      fuzzy = { implementation = "prefer_rust_with_warning" }
+    }
+
+    )
+  end
+  -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+  -- build = 'cargo build --release',
+  -- If you use nix, you can build from source using latest nightly rust with:
+  -- build = 'nix run .#build-plugin',
+}
