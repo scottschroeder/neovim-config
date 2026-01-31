bbb91ef737f3d6a387920e61e46558dbeec587a5
Scott Schroeder <scottschroeder@sent.com>
2026-01-31 13:41:29 -0600

    add github action to run tests

--- tests/minimal_init.lua
+++ tests/minimal_init.lua
@@ -1,3 +1,14 @@
 -- Minimal init for headless test runs with plenary.nvim
 vim.opt.runtimepath:append(".")
-vim.opt.runtimepath:append(vim.fn.stdpath("data") .. "/lazy/plenary.nvim")
+
+local plenary_paths = {
+  vim.fn.getcwd() .. "/plenary.nvim", -- CI
+  vim.fn.stdpath("data") .. "/lazy/plenary.nvim", -- Local
+}
+
+for _, path in ipairs(plenary_paths) do
+  if vim.fn.isdirectory(path) == 1 then
+    vim.opt.runtimepath:append(path)
+    break
+  end
+end
